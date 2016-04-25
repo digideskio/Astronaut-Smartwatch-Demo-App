9c099fa29a63b8e1d2067f1799cdbbc5010befbd
Anton Bredykhin <abredykhin@gmail.com>
2016-04-25 02:03:06 -0700

    Fixed colors in alerts

--- gear/watch/js/alerts.js
+++ gear/watch/js/alerts.js
@@ -86,4 +86,16 @@ angular.module('Watch')
     .controller('AlertDetailCtrl', function ($scope, AppState, Api) {
         $scope.currentData = AppState;
 
+        $scope.getAlertColorClass = function () {
+            var alert = AppState.alert;
+            if (alert.status === 'Advisory') {
+                return 'alert-advisory';
+            } else if (alert.status === 'Warning') {
+                return 'alert-warning';
+            } else if (alert.status === 'Caution') {
+                return 'alert-caution';
+            } else if (alert.status === 'Emergency') {
+                return 'alert-emergency';
+            }
+        };
     });
